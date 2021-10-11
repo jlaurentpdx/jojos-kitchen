2a54d18479aa9397690a73903e5558d9c4f48f9f
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-11 16:26:57 -0700

    initialized spices.js for containing initial offering of spices for store

---
+++ spices.js
@@ -0,0 +1,42 @@
+export const spices = [
+    {
+        id: '1',
+        name: 'cinnamon',
+        description: 'Organic fine-ground cinnamon. Add a dash for that Taco Bell-desert twist... twist.', 
+        quantity: 3,
+        sizeInOz: 2.45,
+        price: 5
+    },
+    {
+        id: '2',
+        name: 'allspice',
+        description: `Yes, I know it does not contain allllll the spices, and no, I don't know why they call it that. Just buy it. Please.`, 
+        quantity: 250,
+        sizeInOz: 3.05,
+        price: 0.5
+    },
+    {
+        id: '3',
+        name: 'nutmeg',
+        description: 'A holiday staple. Believe it or not, this fine spice can be used at other times of the year.', 
+        quantity: 9,
+        sizeInOz: 2.1,
+        price: 4
+    },
+    {
+        id: '4',
+        name: 'cloves',
+        description: 'A little sweet, a little spicy, cloves offer an aromatic and warming sensation to complement the savory dishes in your recipe book.', 
+        quantity: 5,
+        sizeInOz: 1.7,
+        price: 5
+    },
+    {
+        id: '2',
+        name: 'star-anise',
+        description: 'Popular in Chinese and Vietnamese cuisine. The fennel and licorice profile can work wonders with braised meats, fish or poultry, and root vegetables.', 
+        quantity: 1,
+        sizeInOz: 0.5,
+        price: 8
+    }
+];
