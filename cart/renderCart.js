daf1c3f5f5c0f98c121be8e0541c1f96f30c0114
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-12 16:55:17 -0700

    cart now correctly displays order total

--- cart/renderCart.js
+++ cart/renderCart.js
@@ -1,6 +1,6 @@
 import { spices } from '../data/spices.js';
 import { cart } from '../data/cart-data.js';
-import { findById } from '../utils.js';
+import { findById, calculateOrderTotal, toUSD } from '../utils.js';
 import { renderLineItems } from '../render-line-items.js';
 
 const tbody = document.getElementById('table-body');
@@ -11,3 +11,8 @@ for (let cartItem of cart) {
     const tr = renderLineItems(cartItem, spiceData);
     tbody.appendChild(tr);
 }
+
+const orderTotal = calculateOrderTotal(cart, spices);
+const tdOrderTotal = document.getElementById('order-total');
+
+tdOrderTotal.textContent = toUSD(orderTotal);
