7a1e7ed039cfeb571e5b19884d12bd1ef5ec7d8f
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-11 21:23:33 -0700

    created basic CSS styles for page and spice cards

--- test/example.test.js
+++ test/example.test.js
@@ -8,10 +8,11 @@ test('renderSpice should return HTML snippet', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = `<li id="1" class="spice-card"><h2>Cinnamon</h2><img src="./assets/cinnamon.jpg"><p>Organic fine-ground cinnamon. Add a dash for that Taco Bell-desert twist... twist.</p><p>In stock: 3</p><p>Price: $5</p></li>`;
+    const cinnamon = spices[0];
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderSpice(spices[0]).outerHTML;
+    const actual = renderSpice(cinnamon).outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
