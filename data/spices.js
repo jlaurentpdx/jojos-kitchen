cf45dc91e9eef435f341893c24bbfe8d972c1dce
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-16 23:37:14 -0700

    added add product form for admin/index.html

--- data/spices.js
+++ data/spices.js
@@ -4,7 +4,6 @@ export const spices = [
         name: 'Cinnamon',
         img: '../jojos-kitchen/assets/cinnamon.jpg',
         description: 'Organic fine-ground cinnamon. Add a dash for that Taco Bell-desert twist... twist.', 
-        sizeInOz: 2.45,
         price: 5
     },
     {
@@ -12,7 +11,6 @@ export const spices = [
         name: 'Allspice',
         img: '../jojos-kitchen/assets/allspice.jpg',
         description: `Yes, I know it does not contain allllll the spices, and no, I don't know why they call it that. Just buy it. Please.`, 
-        sizeInOz: 3.05,
         price: 1
     },
     {
@@ -20,7 +18,6 @@ export const spices = [
         name: 'Nutmeg',
         img: '../jojos-kitchen/assets/nutmeg.jpg',
         description: 'A holiday staple. Believe it or not, this fine spice can be used at other times of the year.', 
-        sizeInOz: 2.1,
         price: 4
     },
     {
@@ -28,7 +25,6 @@ export const spices = [
         name: 'Cloves',
         img: '../jojos-kitchen/assets/cloves.jpg',
         description: 'A little sweet, a little spicy, cloves offer an aromatic and warming sensation to complement the savory dishes in your recipe book.', 
-        sizeInOz: 1.7,
         price: 5
     },
     {
@@ -36,7 +32,6 @@ export const spices = [
         name: 'Star Anise',
         img: '../jojos-kitchen/assets/star-anise.jpg',
         description: 'Popular in Chinese and Vietnamese cuisine. The fennel and licorice profile can work wonders with braised meats, fish or poultry, and root vegetables.', 
-        sizeInOz: 0.5,
         price: 8
     }
 ];
